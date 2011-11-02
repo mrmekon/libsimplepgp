bebb40b787b70f3b21cfa0bd8d8b793125b7500c
mrmekon <none@none>
2011-11-01 23:05:39 -0400

    Checked in project

---
+++ packet.c
@@ -0,0 +1,16 @@
+/*
+ *  packet.c
+ *  libsimplepgp
+ *
+ *  Created by Trevor Bentley on 11/1/11.
+ *  Copyright 2011 Trevor Bentley. All rights reserved.
+ *
+ */
+
+#include "packet.h"
+
+#include <stdio.h>
+
+void tsb_test(void) {
+	printf("This is a test library.");
+}
---
+++ packet.h
@@ -0,0 +1,10 @@
+/*
+ *  packet.h
+ *  libsimplepgp
+ *
+ *  Created by Trevor Bentley on 11/1/11.
+ *  Copyright 2011 Trevor Bentley. All rights reserved.
+ *
+ */
+
+void tsb_test(void);
