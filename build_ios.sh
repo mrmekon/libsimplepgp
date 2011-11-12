4773f326a57308d5e6fbc7833c1b5cea1dbcd3f9
mrmekon <none@none>
2011-11-11 20:11:00 -0500

    iOS script downloads ios-static libraries

--- build_ios.sh
+++ build_ios.sh
@@ -1,5 +1,21 @@
 #!/bin/sh
 
+IOS_LIB_URL="http://ios-static-libraries.googlecode.com/files/ios-libraries-2011-03-10-065803.zip"
+IOS_LIB_FILE="ios-libraries-2011-03-10-065803.zip"
+
+if [ ! -d "Binaries" ]; then
+    wget ${IOS_LIB_URL} > /dev/null
+    if [ ! -e ${IOS_LIB_FILE} ]; then
+	echo "FAILED TO DOWNLOAD IOS LIBRARIES.  CANNOT CONTINUE."
+	exit 1
+    fi
+    unzip ${IOS_LIB_FILE} > /dev/null
+    if [ $? -ne 0 ]; then
+	echo "Unzip failed.  CANNOT CONTINUE."
+	exit 1
+    fi
+fi
+
 echo "Building iOS library..."
 xcodebuild -project simplepgp.xcodeproj -alltargets > /dev/null
 echo "Building iOS simulator library..."
