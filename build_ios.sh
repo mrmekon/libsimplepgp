50572af345cf9a8d3d9985eff67bf8946fb7f01e
mrmekon <none@none>
2011-11-12 21:20:36 -0500

    Build system changed.  Builds iOS libraries from Xcode or command line, and matched to the correct library for the architecture.  Automatically downloads ios-static libraries from Google Code if necessary

--- build_ios.sh
+++ build_ios.sh
@@ -16,10 +16,22 @@ if [ ! -d "Binaries" ]; then
     fi
 fi
 
-echo "Building iOS library..."
-xcodebuild -project simplepgp.xcodeproj -alltargets > /dev/null
-echo "Building iOS simulator library..."
+
+echo "Building iOS library (armv6)..."
+xcodebuild ARCHS="armv6" -project simplepgp.xcodeproj -alltargets \
+    -configuration "Release armv6" > /dev/null
+if [ $? -ne 0 ]; then exit 1; fi;
+
+echo "Building iOS library (armv7)..."
+xcodebuild ARCHS="armv7" -project simplepgp.xcodeproj -alltargets \
+    -configuration "Release armv7" > /dev/null
+if [ $? -ne 0 ]; then exit 1; fi;
+
+echo "Building iOS library (simulator)..."
 SIMSDK=`xcodebuild -showsdks |grep iphonesim |tail -1 |sed 's/.*-sdk \(.*\)/\1/'`
-xcodebuild -project simplepgp.xcodeproj -alltargets -sdk ${SIMSDK} >/dev/null
+xcodebuild ARCHS="i386" -project simplepgp.xcodeproj -alltargets -sdk ${SIMSDK} \
+    -configuration "Debug simulator" > /dev/null
+if [ $? -ne 0 ]; then exit 1; fi;
+
 echo "Done!"
 
